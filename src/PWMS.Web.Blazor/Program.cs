a0c849120afc4f021bfdad4ea04b6838762f0958
Tótok Patrik <totokpatrik@gmail.com>
2024-11-11 17:54:24 +0100

    adding initial config for blazor client

--- src/PWMS.Web.Blazor/Program.cs
+++ src/PWMS.Web.Blazor/Program.cs
@@ -1,18 +1,30 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using PWMS.Web.Blazor;
+using PWMS.Web.Blazor.Identity;
 using PWMS.Web.Blazor.Services.AuthService;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
 
 // set base address for default host
 builder.Services.AddScoped(sp =>
     new HttpClient { BaseAddress = new Uri(builder.Configuration["BackendUrl"] ?? "https://localhost:5001") });
+
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+
+// adding localstorage
+builder.Services.AddBlazoredLocalStorage();
+
+// adding mud blazor
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
