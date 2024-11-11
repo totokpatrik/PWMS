fcc401b5aea62c2c0492826f6757fc02b8d846ff
Tótok Patrik <totokpatrik@gmail.com>
2024-11-11 20:07:59 +0100

    creating login page

--- src/PWMS.Web.Blazor/Pages/Auth/LoginComponent.razor.cs
+++ src/PWMS.Web.Blazor/Pages/Auth/LoginComponent.razor.cs
@@ -1,9 +1,25 @@
+using FluentValidation;
+using MudBlazor;
+using PWMS.Application.Auth.Models;
+using PWMS.Web.Blazor.Services;
+
 namespace PWMS.Web.Blazor.Pages.Auth
 {
     public partial class LoginComponent
     {
+        MudForm form;
+        bool success;
+        LoginDto model = new LoginDto();
 
-        void Login()
+        FluentValueValidator<string> userNameValidator = new FluentValueValidator<string>(x => x
+            .NotEmpty()
+            .Length(1, 100));
+
+        FluentValueValidator<string> passwordValidator = new FluentValueValidator<string>(x => x
+            .NotEmpty()
+            .Length(1, 100));
+
+        private async Task Login()
         {
             AuthService.Login(new Application.Auth.Models.LoginDto { Password = "test", UserName = "test" });
         }
